00cf32362222fcb3aef44e3178e3196508e41378
wfmpro <wfmpro@outlook.com>
2015-09-23 20:30:48 -0300

    frontend backend
    frontend e backend pra feira

---
+++ SACNET/SACNET/Simulador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace SACNET
+{
+    class Simulador
+    {
+        public string Resposta { get; set; }     
+        public async Task Conectar()
+        {
+            HttpClient c = new HttpClient();
+            Uri location = new Uri("http://localhost/simuladorApi.php");
+            HttpResponseMessage msg = await c.GetAsync(location);
+            if (msg.IsSuccessStatusCode)
+            {
+                Resposta = msg.ToString();
+            }
+        }
+        
+        public void Conectarr()
+        {
+
+        }
+    }
+}
